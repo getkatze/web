cb854943ecceb7d099aa606eaa92c09d721be952
tejasag <tejasagarwalbly@gmail.com>
2021-05-01 07:47:38 +0530

    added urql support

--- src/js/fetch.ts
+++ src/js/fetch.ts
@@ -1,7 +1,24 @@
-// TODO: add gql support
+import { createClient } from "@urql/core";
 
-const fetch = (baseURL) => {
+const client = createClient({
+    url: "https://dependent-hydrant-production.up.railway.app/graphql"
+})
 
+
+export const query = async (query: string, options?: any) => {
+    let bruh = await client.query(query, options || {}).toPromise()
+    return bruh
+    // bruh moment
+
+
+
+
+
+
+
+
+
+
+
+    // get it? *bruh* moment 
 }
-
-export default fetch;
