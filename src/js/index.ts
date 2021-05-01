7bf2d863a2b54239cae4cb455fbb4e1b71224e87
Aiden Bai <mail.contraband@gmail.com>
2021-05-01 11:42:45 -0700

    add senti and predict

--- src/js/index.ts
+++ src/js/index.ts
@@ -5,6 +5,66 @@ import Turbolinks from 'turbolinks';
 Turbolinks.start();
 init();
 
+const predict = async (campaignId: string, text: string): Promise<string> => {
+  const campaign = await query(
+    `
+      query {
+        getCampaignById(campaignId: $id) {
+          tasks options
+        }
+      }
+      `,
+    { id: campaignId }
+  );
+  const completedTasks = campaign.data.tasks.filter((t) => t.completed);
+
+  if (completedTasks.length > 0) {
+    await query(
+      `
+        mutation {
+          clearClassifiers
+        }
+      `
+    );
+    const classifications = [];
+    completedTasks.forEach((task) => {
+      classifications.push([task.category, task.text]);
+    });
+    await query(
+      `
+        mutation {
+          addClassifiers(text: $classsifications)
+        }
+      `,
+      { classifications }
+    );
+  }
+
+  const { data } = await query(
+    `
+      {
+        classify(sentence: $sentence)
+      }
+    `,
+    { sentence: text }
+  );
+  return data.classify;
+};
+
+const sentiment = async (text: string): Promise<string> => {
+  const { data } = await query(
+    `
+    {
+      sentiment(language: "English", sentence: $text)
+    }
+  `,
+    { text }
+  );
+  if (data.sentiment > 0) return 'Positive';
+  if (data.sentiment < 0) return 'Negative';
+  return 'Neutral';
+};
+
 const campaignsState = {
   campaigns: [],
   async setCampaigns() {
@@ -45,18 +105,18 @@ const signup = async (username: string, password: string) => {
 };
 
 interface campaigns {
-  id: string,
-  name: string,
-  contractor: string,
-  description: string,
-  totalPrizeAmount?: number,
-  participantLimit?: number,
-  completed: boolean,
+  id: string;
+  name: string;
+  contractor: string;
+  description: string;
+  totalPrizeAmount?: number;
+  participantLimit?: number;
+  completed: boolean;
 }
 
 const ownedCampaigns = async (name: string): Promise<campaigns[]> => {
   let { data } = await query(
-  `
+    `
   query {
     campaigns(user: $name){
       id
@@ -77,7 +137,7 @@ const ownedCampaigns = async (name: string): Promise<campaigns[]> => {
 
 const login = async (username: string, password: string): Promise<boolean> => {
   let data = await query(
-  `
+    `
   query {
     login(username: $name, password: $pass) {
       id
